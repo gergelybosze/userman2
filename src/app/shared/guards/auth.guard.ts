0fbd66f385a59e03241be7a644784e7b95b4d77b
gergelybosze <gergely.bosze@gmail.com>
2017-05-25 23:47:36 +0200

    auth.guard.ts: @Injectable() added to AdminAuthGuard

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,7 +1,11 @@
 /**
  * Release notes:
- *     20170525 - BoszeG
+ *     v0.3 - 20170525 - BoszeG
+ *        @Injectable() added to AdminAuthGuard
+ *     v0.2 - 20170525 - BoszeG
  *        AdminAuthGuard
+ *     v0.1 - 20170523 - BoszeG
+ *        base
  */
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
@@ -9,7 +13,6 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angul
 import { AuthService } from '../shared.barrel';
 
 @Injectable()
-
 export class AuthGuard implements CanActivate {
   
   public constructor(protected _authService: AuthService) {
@@ -22,6 +25,7 @@ export class AuthGuard implements CanActivate {
 
 }
 
+@Injectable()
 export class AdminAuthGuard implements CanActivate {
   
   public constructor(protected _authService: AuthService) {
@@ -29,7 +33,7 @@ export class AdminAuthGuard implements CanActivate {
   }
 
   public canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    if (this._authService.user.email.search("admin") == -1)
+    if ( this._authService.user.email.search("admin") == -1 )
       return false
     else
       return true;
